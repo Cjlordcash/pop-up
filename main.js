860db912a4b4ee19596f9f5d694ab9c06574d927
Cjlordcash <167579383+Cjlordcash@users.noreply.github.com>
2024-12-30 12:37:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var popUp = document.getElementById("popUp");
+
+function openPopup() {
+  popUp.classList.add("open-popup");
+}
+
+function closePopup() {
+  popUp.classList.remove("open-popup");
+}
